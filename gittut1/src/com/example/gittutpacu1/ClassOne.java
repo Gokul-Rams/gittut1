e41f79c8c3f6f1e39675a3d4f239bc8ea83996ae
Gokul Ramasamy <69314159+Gokul-Rams@users.noreply.github.com>
2021-02-28 22:19:36 +0530

    initial commit

---
+++ gittut1/src/com/example/gittutpacu1/ClassOne.java
@@ -0,0 +1,18 @@
+package com.example.gittutpacu1;
+
+public class ClassOne {
+	String clsname;
+	String clsid;
+	public String getClsname() {
+		return clsname;
+	}
+	public void setClsname(String clsname) {
+		this.clsname = clsname;
+	}
+	public String getClsid() {
+		return clsid;
+	}
+	public void setClsid(String clsid) {
+		this.clsid = clsid;
+	}
+}
